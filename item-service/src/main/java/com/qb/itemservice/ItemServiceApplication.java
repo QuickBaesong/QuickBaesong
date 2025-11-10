388148227e4c5f75a599bc9117d81633f7c71aed
kimpompu <domathing@naver.com>
2025-11-10 15:18:24 +0900

    Revert "feat: add 아이템 생성 기능 추가"
    This reverts commit d27c5b14d03a121bd4ededf3c2728afbf592a076.

--- item-service/src/main/java/com/qb/itemservice/ItemServiceApplication.java
+++ item-service/src/main/java/com/qb/itemservice/ItemServiceApplication.java
@@ -2,16 +2,8 @@ package com.qb.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
-@EnableJpaAuditing
-@ComponentScan(basePackages = {"com.qb.common"})
 public class ItemServiceApplication {
 
   public static void main(String[] args) {
