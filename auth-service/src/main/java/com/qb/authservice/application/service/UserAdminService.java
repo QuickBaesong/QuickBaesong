0ca76b1845d5a34f85e766be8adacfb78c114d87
jessi87 <jessiseo87@gmail.com>
2025-11-11 08:05:29 +0900

    feat: 회원 생성 api - UserAdminService 구현 (관리자 전용)

---
+++ auth-service/src/main/java/com/qb/authservice/application/service/UserAdminService.java
@@ -0,0 +1,48 @@
+package com.qb.authservice.application.service;
+
+import com.qb.authservice.application.component.AuthComponent;
+import com.qb.authservice.domain.entity.User;
+import com.qb.authservice.domain.entity.UserRole;
+import com.qb.authservice.domain.repository.UserRepository;
+import com.qb.authservice.presentation.dto.request.SignupRequest;
+import com.qb.authservice.presentation.dto.response.SignupResponse;
+import com.qb.common.enums.ErrorCode;
+import com.qb.common.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+// 어드민 관련된 회원 서비스를 처리하는 계층 (마스터 권한)
+// 특정 회원 조회, 특정 회원의 권한 조정, 특정 회원의 정보 수정, 회원 생성 등
+@Service
+@RequiredArgsConstructor
+public class UserAdminService {
+    private final UserRepository userRepository;
+    private final AuthComponent authComponent;
+
+    @Transactional
+    public SignupResponse createUser(SignupRequest request) {
+        validateMasterRole(request.role());
+        validateDuplicatedUser(request.username());
+        String encodedPassword = authComponent.encodePassword(request.password());
+
+        User user = request.toEntity(encodedPassword, true); // 미리 승인 처리
+        userRepository.save(user);
+
+        return SignupResponse.from(user); // TODO: 리턴 내용 변경
+    }
+
+
+    public void validateDuplicatedUser(String username) {
+        if (userRepository.existsByUsername(username)) {
+            throw new CustomException(ErrorCode.DUPLICATE_ID);
+        }
+    }
+    // MASTER 권한 검증
+    public void validateMasterRole(UserRole role) {
+        if (role != UserRole.MASTER) {
+            throw new CustomException(ErrorCode.INVALID_MASTER_ROLE);
+        }
+    }
+}
+
--- auth-service/src/main/java/com/qb/authservice/presentation/controller/UserController.java
+++ auth-service/src/main/java/com/qb/authservice/presentation/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.qb.authservice.presentation.controller;
 
+import com.qb.authservice.application.service.AuthService;
+import com.qb.authservice.application.service.UserAdminService;
 import com.qb.authservice.application.service.UserService;
+import com.qb.authservice.domain.entity.UserRole;
 import com.qb.authservice.presentation.dto.request.SignupRequest;
 import com.qb.authservice.presentation.dto.response.SignupResponse;
+import com.qb.common.enums.SuccessCode;
+import com.qb.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserAdminService userAdminService;
 
-    @PostMapping("/sign-up")
-    public SignupResponse signup(@Valid @RequestBody SignupRequest request) {
-        return userService.signup(request);
+    @PreAuthorize("hasRole('MASTER')")
+    @PostMapping
+    public ApiResponse<?> createUser(@Valid @RequestBody SignupRequest request) {
+        SignupResponse response = userAdminService.createUser(request);
+        return ApiResponse.of(SuccessCode.CREATED, response);
     }
 }
