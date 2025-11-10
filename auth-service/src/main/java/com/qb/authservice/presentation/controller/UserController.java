97870a70a0c740b2459545c77ffea66ae580cf26
jessi87 <jessiseo87@gmail.com>
2025-11-11 08:06:26 +0900

    feat: 회원 생성 api - UserAdminService 구현 (관리자 전용)

--- auth-service/src/main/java/com/qb/authservice/presentation/controller/UserController.java
+++ auth-service/src/main/java/com/qb/authservice/presentation/controller/UserController.java
@@ -1,22 +1,15 @@
 package com.qb.authservice.presentation.controller;
 
-import com.qb.authservice.application.service.AuthService;
 import com.qb.authservice.application.service.UserAdminService;
 import com.qb.authservice.application.service.UserService;
-import com.qb.authservice.domain.entity.UserRole;
 import com.qb.authservice.presentation.dto.request.SignupRequest;
 import com.qb.authservice.presentation.dto.response.SignupResponse;
 import com.qb.common.enums.SuccessCode;
 import com.qb.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/users")
