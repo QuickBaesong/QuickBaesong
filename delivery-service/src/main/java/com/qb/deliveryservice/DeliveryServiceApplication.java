fef77626c7a6f225a7a90f813f13e02dc720dba5
banasu0723 <banasu0723@gmail.com>
2025-11-11 03:58:12 +0900

    feat: 배송 Application 어노테이션 추가

--- delivery-service/src/main/java/com/qb/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/qb/deliveryservice/DeliveryServiceApplication.java
@@ -2,8 +2,16 @@ package com.qb.deliveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableJpaAuditing
+@ComponentScan(basePackages = {"com.qb.deliveryservice", "com.qb.common"})
 public class DeliveryServiceApplication {
 
   public static void main(String[] args) {
