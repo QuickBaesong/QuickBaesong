1444c95652d6d1289457b0094eaa673d76119ec4
banasu0723 <banasu0723@gmail.com>
2025-11-11 14:36:57 +0900

    fix: 배송 생성 응답 ApiResponse 로 변경

--- delivery-service/src/main/java/com/qb/deliveryservice/presentation/controller/DeliveryInternalController.java
+++ delivery-service/src/main/java/com/qb/deliveryservice/presentation/controller/DeliveryInternalController.java
@@ -1,5 +1,7 @@
 package com.qb.deliveryservice.presentation.controller;
 
+import com.qb.common.enums.SuccessCode;
+import com.qb.common.response.ApiResponse;
 import com.qb.deliveryservice.application.dto.DeliveryCreateRequest;
 import com.qb.deliveryservice.application.dto.DeliveryResponse;
 import com.qb.deliveryservice.application.service.DeliveryService;
@@ -16,8 +18,10 @@ public class DeliveryInternalController {
     private final DeliveryService deliveryService;
 
     @PostMapping
-    public ResponseEntity<DeliveryResponse> createDelivery(@RequestBody DeliveryCreateRequest req) {
+    public ResponseEntity<ApiResponse<DeliveryResponse>> createDelivery(@RequestBody DeliveryCreateRequest req) {
         DeliveryResponse resp = deliveryService.create(req);
-        return ResponseEntity.ok(resp);
+        return ResponseEntity.ok(
+                ApiResponse.of(SuccessCode.CREATED, resp)
+        );
     }
 }
