65108b7d4f87066f9d112ceb309e8a68bcf66b3f
trevivom76 <trevivom76@gmail.com>
2025-11-11 20:33:19 +0900

    feat: 업체 생성 기능 구현 완료 - api 별로 브랜치 생성을 위해 조회 api 관련 코드 삭제 - Application 내 패키지 스캔 명시화 (companyservice 추가)

--- company-service/src/main/java/com/qb/companyservice/application/service/CompanyDomainService.java
+++ company-service/src/main/java/com/qb/companyservice/application/service/CompanyDomainService.java
@@ -4,6 +4,7 @@ import com.qb.companyservice.domain.entity.Company;
 import com.qb.companyservice.domain.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,21 +39,6 @@ public class CompanyDomainService {
   }
 
   /**
-   * 업체 ID로 업체 조회
-   * @param companyId 업체 ID
-   * @return 업체 엔티티
-   */
-  public Company findById(UUID companyId) {
-    log.debug("업체 조회 시작: ID={}", companyId);
-
-    return companyRepository.findByCompanyIdAndDeletedAtIsNull(companyId)
-        .orElseThrow(() -> {
-          log.warn("업체를 찾을 수 없습니다: ID={}", companyId);
-          return new IllegalArgumentException("업체를 찾을 수 없습니다: " + companyId);
-        });
-  }
-
-  /**
    * 업체명 중복 검증
    * @param companyName 검증할 업체명
    * @param excludeCompanyId 제외할 업체 ID (수정 시 자기 자신 제외)
@@ -65,14 +51,5 @@ public class CompanyDomainService {
       log.warn("업체명이 이미 존재합니다: {}", companyName);
       throw new IllegalArgumentException("이미 존재하는 업체명입니다: " + companyName);
     }
-  }
-
-  /**
-   * 특정 허브에 속한 업체 수 조회
-   * @param hubId 허브 ID
-   * @return 업체 수
-   */
-  public long countByHubId(UUID hubId) {
-    return companyRepository.countByHubIdAndDeletedAtIsNull(hubId);
   }
 }
