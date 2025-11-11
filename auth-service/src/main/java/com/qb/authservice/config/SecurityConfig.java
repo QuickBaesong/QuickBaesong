b445ca7496baa4f4481648f62ad2448f3794625f
jessi87 <jessiseo87@gmail.com>
2025-11-11 09:06:07 +0900

    feat: security 구성 - 우선 auth 모든 요청 허용

--- auth-service/src/main/java/com/qb/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/qb/authservice/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.qb.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // 모든 요청을 허용 (인증은 Interceptor에서 처리)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
 
     // 비밀번호 암호화
     @Bean
