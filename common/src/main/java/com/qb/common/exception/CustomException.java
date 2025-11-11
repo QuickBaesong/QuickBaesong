be21201fb5d67fe968b9742d1a4f70a78e31a66a
jessi87 <jessiseo87@gmail.com>
2025-11-11 16:50:56 +0900

    refactor: 공통 exception 처리 구조 변경 - 각 서비스 별로 ErrorCode 분리 (BaseErrorCode 상속)

--- common/src/main/java/com/qb/common/exception/CustomException.java
+++ common/src/main/java/com/qb/common/exception/CustomException.java
@@ -1,16 +1,15 @@
 package com.qb.common.exception;
 
-import com.qb.common.enums.ErrorCode;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException{
-    private final ErrorCode errorCode;
+    private final BaseErrorCode errorCode;
     private final String message;
     private final HttpStatus status;
 
-    public CustomException(ErrorCode errorCode){
+    public CustomException(BaseErrorCode errorCode){
         super(errorCode.getMessage());
         this.errorCode = errorCode;
         this.message = errorCode.getMessage();
