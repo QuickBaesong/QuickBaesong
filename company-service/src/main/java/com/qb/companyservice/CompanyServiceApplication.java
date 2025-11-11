a46db4df73ae53327a68030b39e883308de321d2
trevivom76 <trevivom76@gmail.com>
2025-11-11 19:40:17 +0900

    feat: 의존성 추가, application 추가 - common 모듈 의존성 - JPA, validation, h2 의존성 추가 - 멀티프로필 (local, test)

--- company-service/src/main/java/com/qb/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/com/qb/companyservice/CompanyServiceApplication.java
@@ -2,12 +2,15 @@ package com.qb.companyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class CompanyServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CompanyServiceApplication.class, args);
   }
-
 }
