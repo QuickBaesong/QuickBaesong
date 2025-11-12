69786ab2da4f9a403a18ce619d5f701f647858b1
이호준 <jake525@naver.com>
2025-11-12 17:26:41 +0900

    Feat : GetHubRouteResponse dto 필드 생성 및 추가

--- hub-service/src/main/java/com/qb/hubservice/presentation/response/GetHubRouteResponse.java
+++ hub-service/src/main/java/com/qb/hubservice/presentation/response/GetHubRouteResponse.java
@@ -1,4 +1,37 @@
 package com.qb.hubservice.presentation.response;
 
+import com.qb.hubservice.domain.model.HubRoute;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class GetHubRouteResponse {
+
+    private final UUID hubRouteId;
+
+    private final UUID startHubId;
+
+    private final UUID destinationHubId;
+
+    private final BigDecimal distance;
+
+    private final Long duration;
+
+    public static GetHubRouteResponse from(HubRoute hubRoute) {
+        return GetHubRouteResponse.builder()
+                .hubRouteId(hubRoute.getHubRouteId())
+                .startHubId(hubRoute.getStartHub().getHubId())
+                .destinationHubId(hubRoute.getDestinationHub().getHubId())
+                .distance(hubRoute.getDistance())
+                .duration(hubRoute.getDuration())
+                .build();
+
+    }
+
 }
