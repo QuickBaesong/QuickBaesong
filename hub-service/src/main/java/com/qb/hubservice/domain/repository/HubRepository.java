3c91e7ca9f8cbf4c5bff39fc0509b0c005c637eb
이호준 <jake525@naver.com>
2025-11-12 15:59:28 +0900

    Feat : repository jpql 적용 - 검색 및 페이징,정렬

--- hub-service/src/main/java/com/qb/hubservice/domain/repository/HubRepository.java
+++ hub-service/src/main/java/com/qb/hubservice/domain/repository/HubRepository.java
@@ -1,9 +1,29 @@
 package com.qb.hubservice.domain.repository;
 
 import com.qb.hubservice.domain.model.Hub;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.UUID;
 
 public interface HubRepository extends JpaRepository<Hub, UUID> {
+
+    Page<Hub> findByDeletedAtIsNull(Pageable pageable);
+
+    @Query("SELECT h FROM Hub h " +
+            "WHERE h.deletedAt IS NULL " + // 논리적 삭제 조건
+            "AND ( " +
+            "    (:hubName IS NULL OR :hubName = '' OR LOWER(h.hubName) LIKE LOWER(CONCAT('%', :hubName, '%'))) " +
+            "    OR " + // 이름 또는 주소 중 하나라도 일치
+            "    (:hubAddress IS NULL OR :hubAddress = '' OR LOWER(h.location.address) LIKE LOWER(CONCAT('%', :hubAddress, '%'))) " +
+            ")")
+    Page<Hub> findByComplexConditions(
+            @Param("hubName") String hubName,
+            @Param("hubAddress") String hubAddress,
+            Pageable pageable
+    );
 
 }
