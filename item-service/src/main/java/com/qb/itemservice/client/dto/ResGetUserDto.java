00e80675e2e59cdd001fcb131a64be7818f24cb5
kimpompu <domathing@naver.com>
2025-11-12 19:57:24 +0900

    feat:add item-service 유저정보조회 FeignClient
    - FeignClient 추가

---
+++ item-service/src/main/java/com/qb/itemservice/client/dto/ResGetUserDto.java
@@ -0,0 +1,14 @@
+package com.qb.itemservice.client.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResGetUserDto {
+	private UUID company;
+}
