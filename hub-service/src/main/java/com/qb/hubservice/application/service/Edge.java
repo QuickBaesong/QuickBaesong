3a2e7f368757329324e7d7273b6f93027803db66
이호준 <jake525@naver.com>
2025-11-13 15:16:19 +0900

    Feat : ShortestRoute 다익스트라 알고리즘에 필요한 Node 객체 생성

--- hub-service/src/main/java/com/qb/hubservice/application/service/Edge.java
+++ hub-service/src/main/java/com/qb/hubservice/application/service/Edge.java
@@ -1,4 +1,14 @@
 package com.qb.hubservice.application.service;
 
-public class Edge {
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class Edge { // default 접근 제한자 (같은 패키지 내에서만 접근 가능)
+    final UUID destinationHubId;
+    final BigDecimal distance;
+    final Long duration;
 }
