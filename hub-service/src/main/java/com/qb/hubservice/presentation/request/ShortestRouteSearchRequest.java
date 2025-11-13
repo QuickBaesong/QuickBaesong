dbfaddb3259bd6849d28e904524fcf4e38047d34
이호준 <jake525@naver.com>
2025-11-13 15:15:17 +0900

    Feat : ShortestRoute 다익스트라 알고리즘에 필요한 edge 객체 생성

--- hub-service/src/main/java/com/qb/hubservice/presentation/request/ShortestRouteSearchRequest.java
+++ hub-service/src/main/java/com/qb/hubservice/presentation/request/ShortestRouteSearchRequest.java
@@ -1,4 +1,23 @@
 package com.qb.hubservice.presentation.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortestRouteSearchRequest {
+
+    @NotNull(message = "출발 허브 ID는 필수입니다.")
+    private UUID startHubId;
+
+
+    @NotNull(message = "도착 허브 ID는 필수입니다.")
+    private UUID endHubId;
+
+
 }
--- hub-service/src/main/java/com/qb/hubservice/presentation/response/ShortestRouteResponse.java
+++ hub-service/src/main/java/com/qb/hubservice/presentation/response/ShortestRouteResponse.java
@@ -1,4 +1,41 @@
 package com.qb.hubservice.presentation.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortestRouteResponse {
+
+    private BigDecimal totalDistance;
+
+
+    private Long totalDuration;
+
+
+    private List<RouteSegment> segments;
+
+    //내부 클래스 -
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RouteSegment {
+
+        private UUID startHubId;
+
+
+        private UUID destinationHubId;
+
+
+        private BigDecimal distance;
+
+
+        private Long duration;
+    }
+
 }
