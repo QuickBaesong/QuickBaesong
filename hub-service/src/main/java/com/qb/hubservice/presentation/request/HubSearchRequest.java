5d35d8ac49c765e920dbff2b3539d4c4307db6bc
이호준 <jake525@naver.com>
2025-11-13 09:28:14 +0900

    Feat : HubSearchRequest validation 추가 및 생성

--- hub-service/src/main/java/com/qb/hubservice/presentation/request/HubSearchRequest.java
+++ hub-service/src/main/java/com/qb/hubservice/presentation/request/HubSearchRequest.java
@@ -1,6 +1,8 @@
 package com.qb.hubservice.presentation.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class HubSearchRequest {
 
+    @NotBlank(message = "허브 이름은 필수입니다.")
+    @Size(max = 255, message = "허브 이름은 255자 이하이어야 합니다.")
     private String hubName;
 
+    @NotBlank(message = "주소는 필수입니다.")
+    @Size(max = 255, message = "주소는 255자 이하이어야 합니다.")
     private String hubAddress;
 
 }
