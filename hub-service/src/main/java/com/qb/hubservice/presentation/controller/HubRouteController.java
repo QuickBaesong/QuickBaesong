c215082f3157aeecccd8ced3fe9753ab40f46ef2
이호준 <jake525@naver.com>
2025-11-13 09:26:20 +0900

    Feat : 단일 정보 조회 서비스 로직 구현 및 추가

--- hub-service/src/main/java/com/qb/hubservice/presentation/controller/HubRouteController.java
+++ hub-service/src/main/java/com/qb/hubservice/presentation/controller/HubRouteController.java
@@ -9,10 +9,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +29,16 @@ public class HubRouteController {
                 .status(HttpStatus.CREATED)
                 .body(ApiResponse.of(SuccessCode.CREATED, response));
     }
+
+    @GetMapping("/{hubRouteId}")
+    public ResponseEntity<ApiResponse<GetHubRouteResponse>> getHubRoute(
+            @PathVariable UUID hubRouteId
+    ) {
+        GetHubRouteResponse response = hubRouteService.getHubRoute(hubRouteId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(SuccessCode.OK, response));
+    }
+
 
 }
